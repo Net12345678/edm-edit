129550b55bd9f069e281fe686787b9906df37c64
lee <white11190709@gmail.com>
2020-03-04 16:52:25 +0800

    create EDMtemplate

--- js/all.js
+++ js/all.js
@@ -1,38 +1,161 @@
-var EDMLayout = new Vue({
-    data:{
-        layoutA:{
-            logo:{
-                imgsrc:'https://fakeimg.pl/640x98/ff548f/000?text=LOGO(640x98)',
-                linkhref:''
-            },
-            banner:{
-                imgsrc:'',
-                linkhref:'',
-                alt:''
-            },
-            product:[{
-                imgsrc:'',
-                linkhref:'',
-                alt:''
-            }],
-            footer:{
-                fb:{
-                    imgsrc:'',
-                    linkhref:''
-                },
-                ig:{
-                    imgsrc:'',
-                    linkhref:''
-                },
-                line:{
-                    imgsrc:'',
-                    linkhref:''
-                },
-                content:{
-                    imgsrc:''
-                }
-            }
-        }
-    }
+var app = new Vue({
+	el: '#app',
+	data: {
+		EDMtemplate: [
+			{
+				logo: {
+					id: 'logo',
+					link: '',
+					imgSrc: '',
+					linkText: 'Logo 連結',
+					imgText: 'Logo 圖片位址',
+				},
+				banner: {
+					id: 'banner',
+					link: '',
+					imgSrc: '',
+					alt: '',
+					linkText: 'Banner 連結',
+					imgText: 'Banner 圖片位址',
+					aliText: 'Banner 描述',
+				},
+				product: [
+					{
+						id: 'pd_1',
+						link: '',
+						imgSrc: '',
+						alt: '',
+						linkText: 'Product_01 連結',
+						imgText: 'Product_01 圖片位址',
+						aliText: 'Product_01 描述',
+					},
+					{
+						id: 'pd_2',
+						link: '',
+						imgSrc: '',
+						alt: '',
+						linkText: 'Product_02 連結',
+						imgText: 'Product_02 圖片位址',
+						aliText: 'Product_02 描述',
+					},
+					{
+						id: 'pd_3',
+						link: '',
+						imgSrc: '',
+						alt: '',
+						linkText: 'Product_03 連結',
+						imgText: 'Product_03 圖片位址',
+						aliText: 'Product_03 描述',
+					},
+				],
+				footer: {
+					fb: {
+						id: 'fb',
+						link: '',
+						imgSrc: '',
+						alt: 'facebook',
+						linkText: 'Facebook 連結',
+						imgText: 'Facebook 圖片位址',
+					},
+					ig: {
+						id: 'ig',
+						link: '',
+						imgSrc: '',
+						alt: 'instagram',
+						linkText: 'Instagram 連結',
+						imgText: 'Instagram 圖片位址',
+					},
+					line: {
+						id: 'line',
+						link: '',
+						imgSrc: '',
+						alt: 'line',
+						linkText: 'Line 連結',
+						imgText: 'Line 圖片位址',
+					},
+					content: {
+						id: 'footerContent',
+						link: '',
+						imgSrc: '',
+						linkText: 'Footer 連結',
+						imgText: 'Footer 圖片位址',
+					}
+				}
+			},
+			{
+				logo: {
+					id: 'logo',
+					link: '',
+					imgSrc: '',
+					linkText: 'Logo 連結',
+					imgText: 'Logo 圖片位址',
+				},
+				banner: {
+					id: 'banner',
+					link: '',
+					imgSrc: '',
+					alt: '',
+					linkText: 'Banner 連結',
+					imgText: 'Banner 圖片位址',
+					aliText: 'Banner 描述',
+				},
+				product: [
+					{
+						id: 'pd_1',
+						link: '',
+						imgSrc: '',
+						alt: '',
+						linkText: 'Product_01 連結',
+						imgText: 'Product_01 圖片位址',
+						aliText: 'Product_01 描述',
+					},
+					{
+						id: 'pd_2',
+						link: '',
+						imgSrc: '',
+						alt: '',
+						linkText: 'Product_02 連結',
+						imgText: 'Product_02 圖片位址',
+						aliText: 'Product_02 描述',
+					},
+				],
+				footer: {
+					fb: {
+						id: 'fb',
+						link: '',
+						imgSrc: '',
+						alt: 'facebook',
+						linkText: 'Facebook 連結',
+						imgText: 'Facebook 圖片位址',
+					},
+					ig: {
+						id: 'ig',
+						link: '',
+						imgSrc: '',
+						alt: 'instagram',
+						linkText: 'Instagram 連結',
+						imgText: 'Instagram 圖片位址',
+					},
+					line: {
+						id: 'line',
+						link: '',
+						imgSrc: '',
+						alt: 'line',
+						linkText: 'Line 連結',
+						imgText: 'Line 圖片位址',
+					},
+					content: {
+						id: 'footerContent',
+						link: '',
+						imgSrc: '',
+						linkText: 'Footer 連結',
+						imgText: 'Footer 圖片位址',
+					}
+				}
+			},
+		],
+	},
+
+
 });
 
