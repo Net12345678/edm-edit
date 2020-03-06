a3d6bec54b3e89a97b8714fc5e58410cc0eb96bf
Hui Hong Chang <stagbeetlehui@gmail.com>
2020-03-06 15:24:52 +0800

    add v-if

--- js/all.js
+++ js/all.js
@@ -45,76 +45,6 @@ var app = new Vue({
 					imgSrc:'https://fakeimg.pl/640x150/f00d30/000?text=BUTTON',
 					link:'#',						
 				},
-				product:[{
-						imgSrc:'',
-						link:'',
-						alt:''
-				}],
-				footer:{
-						fb:{
-								imgSrc:'',
-								link:''
-						},
-						ig:{
-								imgSrc:'',
-								link:''
-						},
-						line:{
-								imgSrc:'',
-								link:''
-						}
-				},
-				footerContent: {
-					id: 'footerContent',
-					imgSrc: 'https://fakeimg.pl/460x158/000000/fff?text=footer-content(460x158)',
-				}
-		},
-
-		EDMtemplate: [
-			{
-				logo: {
-					id: 'logo',
-					link: 'https://www.nuvfashion.com/',
-					imgSrc: '',
-				},
-				menu: [{
-					id: 'women',
-					link: '#',
-					imgSrc: 'https://fakeimg.pl/200x88/ffffff/000?text=WOMEN',
-					alt:'women',
-					width: '200'
-				},
-				{
-					id: 'men',
-					link: '#',
-					imgSrc: 'https://fakeimg.pl/120x88/ffffff/000?text=MEN',
-					alt:'men',
-					width: '120'
-				},
-				{
-					id: 'kids',
-					link: '#',
-					imgSrc: 'https://fakeimg.pl/145x88/ffffff/000?text=KIDS',
-					alt:'kids',
-					width: '145'
-				},
-				{
-					id: 'baby',
-					link: '#',
-					imgSrc: 'https://fakeimg.pl/175x88/ffffff/000?text=BABY',
-					alt:'baby',
-					width: '175'
-				}
-				],
-				banner: {
-					id: 'banner',
-					link: '',
-					imgSrc: '',
-				},
-				button:{
-					link:'',
-					imgSrc:'',						
-				},
 				product: [
 					{
 						id: 'pd_1',
@@ -165,23 +95,138 @@ var app = new Vue({
 						alt: 'product08',						
 					}
 				],
+				footer:{
+						fb:{
+								imgSrc:'https://fakeimg.pl/60x158/000000/fff?text=fb(60x158)',
+								link:''
+						},
+						ig:{
+								imgSrc:'https://fakeimg.pl/60x158/000000/fff?text=ig(60x158)',
+								link:''
+						},
+						line:{
+								imgSrc:'https://fakeimg.pl/60x158/000000/fff?text=line(60x158)',
+								link:''
+						}
+				},
+				footerContent: {
+					id: 'footerContent',
+					imgSrc: 'https://fakeimg.pl/460x158/000000/fff?text=footer-content(460x158)',
+				}
+		},
+
+		EDMtemplate: [
+			{
+				logo: {
+					id: 'logo',
+					link: 'https://www.nuvfashion.com/',
+					imgSrc: '',
+				},
+				menu: [{
+					id: 'women',
+					link: '#',
+					imgSrc: '',
+					alt:'women',
+					width: '200'
+				},
+				{
+					id: 'men',
+					link: '#',
+					imgSrc: '',
+					alt:'men',
+					width: '120'
+				},
+				{
+					id: 'kids',
+					link: '#',
+					imgSrc: '',
+					alt:'kids',
+					width: '145'
+				},
+				{
+					id: 'baby',
+					link: '#',
+					imgSrc: '',
+					alt:'baby',
+					width: '175'
+				}
+				],
+				banner: {
+					id: 'banner',
+					link: '',
+					imgSrc: '',
+				},
+				button:{
+					link:'',
+					imgSrc:'',						
+				},
+				product: [
+					{
+						id: 'pd_1',
+						link: '#',
+						imgSrc: '',
+						alt: 'product01',						
+					},
+					{
+						id: 'pd_2',
+						link: '#',
+						imgSrc: '',
+						alt: 'product02',						
+					},
+					{
+						id: 'pd_3',
+						link: '#',
+						imgSrc: '',
+						alt: 'product03',
+					},
+					{
+						id: 'pd_4',
+						link: '#',
+						imgSrc: '',
+						alt: 'product04',						
+					},
+					{
+						id: 'pd_5',
+						link: '#',
+						imgSrc: '',
+						alt: 'product05',						
+					},
+					{
+						id: 'pd_6',
+						link: '#',
+						imgSrc: '',
+						alt: 'product06',						
+					},
+					{
+						id: 'pd_7',
+						link: '#',
+						imgSrc: '',
+						alt: 'product07',						
+					},
+					{
+						id: 'pd_8',
+						link: '#',
+						imgSrc: '',
+						alt: 'product08',						
+					}
+				],
 				footerIcon: {
 					fb: {
 						id: 'fb',
 						link: '#',
-						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=fb(60x158)',
+						imgSrc: '',
 						alt: 'facebook',
 					},
 					ig: {
 						id: 'ig',
 						link: '#',
-						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=ig(60x158)',
+						imgSrc: '',
 						alt: 'instagram',
 					},
 					line: {
 						id: 'line',
 						link: '#',
-						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=line(60x158)',
+						imgSrc: '',
 						alt: 'line',
 					}					
 				},
