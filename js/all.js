0498d76ce886eba16fb8bcb63d72ed564afde389
lee <white11190709@gmail.com>
2020-03-06 11:14:55 +0800

    第一版完成

--- js/all.js
+++ js/all.js
@@ -1,3 +1,30 @@
+// var EDMinput = {
+// 	data: function () {
+//     return {
+      
+//     }
+// 	},
+// 	props: [
+// 		'id',
+// 		'title',
+// 		'link',
+// 		'img',
+// 	],
+// 	template: `
+// 	<div class="form-group">
+// 		<label class="mb-2" for="${id}">${title}</label>
+// 		<div class="row">
+// 			<div class="col">
+// 				<input type="text" v-model="${link}" class="form-control" placeholder="${title}">
+// 			</div>
+// 			<div class="col">
+// 				<slot></slot>
+// 			</div>
+// 		</div>
+// 	</div>
+// 	`,
+// };
+
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -190,57 +217,77 @@ var app = new Vue({
 			},
 			{
 				logo: {
-					id: 'logo2',
-					link: '',
+					id: 'logo',
+					link: 'https://www.nuvfashion.com/',
 					imgSrc: '',
 				},
+				menu: [
+					{
+						id: 'women',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/200x88/ffffff/000?text=WOMEN',
+						alt:'women',
+						width: '200'
+					},
+					{
+						id: 'men',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/120x88/ffffff/000?text=MEN',
+						alt:'men',
+						width: '120'
+					},
+					{
+						id: 'kids',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/145x88/ffffff/000?text=KIDS',
+						alt:'kids',
+						width: '145'
+					},
+					{
+						id: 'baby',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/175x88/ffffff/000?text=BABY',
+						alt:'baby',
+						width: '175'
+					}
+				],
 				banner: {
 					id: 'banner',
 					link: '',
 					imgSrc: '',
-					alt: '',
-					aliText: 'Banner 描述',
 				},
+				button:'',
 				product: [
 					{
 						id: 'pd_1',
-						link: '',
-						imgSrc: '',
-						alt: '',
-						aliText: 'Product_01 描述',
-					},
-					{
-						id: 'pd_2',
-						link: '',
-						imgSrc: '',
-						alt: '',
-						aliText: 'Product_02 描述',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/640x859/00154d?text=product',
+						alt: 'product01',						
 					},
 				],
-				footer: {
+				footerIcon: {
 					fb: {
 						id: 'fb',
-						link: '',
-						imgSrc: '',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=fb(60x158)',
 						alt: 'facebook',
 					},
 					ig: {
 						id: 'ig',
-						link: '',
-						imgSrc: '',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=ig(60x158)',
 						alt: 'instagram',
 					},
 					line: {
 						id: 'line',
-						link: '',
-						imgSrc: '',
+						link: '#',
+						imgSrc: 'https://fakeimg.pl/60x158/000000/fff?text=line(60x158)',
 						alt: 'line',
-					},
-					content: {
-						id: 'footerContent',
-						link: '',
-						imgSrc: '',
-					}
+					}					
+				},
+				footerContent: {
+					id: 'footerContent',
+					imgSrc: '',
 				}
 			},
 		],
@@ -248,128 +295,8 @@ var app = new Vue({
 		nowEDMTemplate: '',
 		nowEDMTemplateIndex: '',
 		EDMForm: [
-			`
-			<!-- logo -->
-            <div class="form-group">
-              <label class="mb-2" for="logo_link">Logo 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.logo.link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.logo.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-            <!-- menu -->
-            <div class="form-group">
-              <label class="mb-2" for="women_link">WOMEN 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[0].link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[0].imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="mb-2" for="men_link">MEN 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[1].link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[1].imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="mb-2" for="kids_link">KIDS 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[2].link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[2].imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="mb-2" for="baby_link">BABY 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[3].link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.menu[3].imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-
-            <!-- banner -->
-            <div class="form-group">
-              <label class="mb-2" for="banner_link">Banner 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.banner.link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.banner.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-
-            <!-- button -->
-            <div class="form-group">
-              <label class="mb-2" for="button_link">Button 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.button.link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.button.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-
-            <!-- product -->
-            <div v-for="(product, index) in nowEDMTemplate.product" class="form-group">
-              <label class="mb-2" for="product_link">Product{{index + 1}} 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="product.link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="product.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-
-            <!-- footer-icon -->
-            <div v-for="footerIcon in nowEDMTemplate.footerIcon"  class="form-group">
-              <label class="mb-2" for="footerIcon_link">{{footerIcon.id}} 連結 / 圖片</label>
-              <div class="row">
-                <div class="col">
-                  <input type="text" v-model="footerIcon.link" class="form-control" placeholder="連結">
-                </div>
-                <div class="col">
-                  <input type="text" v-model="footerIcon.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-
-            <!-- footer-content -->
-            <div class="form-group">
-              <label class="mb-2" for="footerContent_link">footerContent 連結 / 圖片</label>
-              <div class="row">                
-                <div class="col">
-                  <input type="text" v-model="nowEDMTemplate.footerContent.imgSrc" class="form-control" placeholder="圖片位址">
-                </div>
-              </div>
-            </div>
-			`,
-			`1`,
+			`1版`,
+			`2版`,
 		],
 		oupputData: '',
 	},
@@ -389,9 +316,13 @@ var app = new Vue({
 	computed: {
 		toNumber() {
 			this.nowEDMTemplateIndex = parseInt(this.nowEDMTemplateIndex);
-		}
-		
+		},
+		refresh() {
+			const edmInput = document.getElementById('edmInput');
+
+		},
 	},
+
 
 });
 
