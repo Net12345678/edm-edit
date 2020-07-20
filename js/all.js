d655f3a52d07c4bdaa3457b0ebe0597b076d721e
lee <white11190709@gmail.com>
2020-07-20 15:57:11 +0800

    更新說明、商品後面加入編號

--- js/all.js
+++ js/all.js
@@ -805,7 +805,7 @@ var app = new Vue({
 			edm.product.forEach((item,key)=>{
 				const link = 'pdLink_' + (key+1);
 				const img = 'pdImg_' + (key+1);
-				item.link = data[0][link] + `#utm${key + 1}`;
+				item.link = data[0][link] + `#utm_${key + 1}`;
 				item.imgSrc = data[0][img];
 			})
 		},
